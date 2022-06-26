40ed1741c8f19d9ca96d0939ab7ec92ed87619e6
Petr <je09@yandex.ru>
2022-06-26 17:17:35 +0500

    project requirements

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+from django.test import (
+    TestCase,
+    Client
+)
+from django.urls import reverse
+from rest_framework import status
+from users.models import User
+from users.serializers import UserSerializer
+from library.models import Book
 
-# Create your tests here.
+client = Client()
+
+
+class TestUserListApiView(TestCase):
+    """
+    Testing GET of /api/v1/users/
+    """
+
+    def setUp(self) -> None:
+        # Create two users with unique vk_id
+        User.objects.create(vk_id=371449298)
+        User.objects.create(vk_id=489302347)
+
+        # Create user with an empty list of books
+        User.objects.create(vk_id=123123123)
+
+        # Create few books linked to the first user
+        Book.objects.create(vk_id=User.objects.get(vk_id=371449298),
+                            title="Kafka on the Shore", author="Haruki Murakami",)
+        Book.objects.create(vk_id=User.objects.get(vk_id=371449298),
+                            title="The Old Man and the Sea", author="Ernest Hemingway", )
+        Book.objects.create(vk_id=User.objects.get(vk_id=371449298),
+                            title="The Trial", author="Kafka Franz", )
+
+        # Create few book linked to the second user
+        Book.objects.create(vk_id=User.objects.get(vk_id=489302347), title="Hyperion", author="Dan Simons",)
+        Book.objects.create(vk_id=User.objects.get(vk_id=489302347), title="Ubik", author="Philip K. Dick", )
+        Book.objects.create(vk_id=User.objects.get(vk_id=489302347),
+                            title="Revelation Space", author="Reynolds Alastair", )
+
+    def test_get_users(self):
+        # get data from API response with vk_id=371449298
+        response = client.get(reverse('api_user:user_info'), data={'vk_id': 371449298})
+
+        # get data from ORM
+        clients = User.objects.get(vk_id=371449298)
+        clients_serialized = UserSerializer(clients)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, clients_serialized.data)
+
+    def test_get_empty_user(self):
+        # get data from API response with vk_id=123123123
+        response = client.get(reverse('api_user:user_info'), data={'vk_id': 123123123})
+
+        # get data from ORM
+        clients = User.objects.get(vk_id=371449298)
+        clients_serialized = UserSerializer(clients)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, clients_serialized.data)
