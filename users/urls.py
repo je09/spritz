6e50a89b2f50b129232c4582a9f8f8af2b9a8ff6
Petr <je09@yandex.ru>
2022-07-03 22:00:41 +0500

    upd: uers.urls unused imports
    remove unused imports

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 from users.views import UserListAPIView
 
 app_name = 'api_user'
 urlpatterns = [
-    path('', UserListAPIView.as_view(), name='user info'),
+    path('', UserListAPIView.as_view(), name='user_info'),
 ]
