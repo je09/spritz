810484455dd22ecdac90e401b10359066ab879f0
Petr <je09@yandex.ru>
2022-07-12 13:24:17 +0500

    upd: library urls for user stats

--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from library.views import (
     LibraryRetrieveViewSet,
+    StatisticsViewSet,
 )
 
 app_name = 'api_library'
 urlpatterns = [
     path('', LibraryRetrieveViewSet.as_view(), name='library_list_control'),
+    path('progress/', StatisticsViewSet.as_view(), name='library_stat_control'),
 ]
