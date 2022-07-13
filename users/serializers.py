c5532185d4e4dbcba22817601541f82441f719f2
Petr <je09@yandex.ru>
2022-07-13 22:31:25 +0500

    upd: user serializer book details
    Retrieve Book info instead of book_id

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from users.models import User
+from library.serializers import BookViewSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
     vk_id = serializers.IntegerField()
-    books_id = serializers.StringRelatedField(many=True)
+    books = BookViewSerializer(many=True, read_only=True)
 
     # TODO: Return forbidden status code if vk_id is not set.
 
     class Meta:
         model = User
-        fields = ('vk_id', 'books_id')
+        fields = ('vk_id', 'books')
