c44d0368d9170f9cb1ba03b38070c42e5293f666
Petr <je09@yandex.ru>
2022-07-15 13:55:18 +0300

    upd: library models – Librarian
    The idea is to have a user with unreachable for normal users id, which is vk_id=0 to make a public bookshelf. The "User" with vk_id=0 is a librarian. The books from his bookshelf can be added to LibrarianJournal to indicate, that somebody copied them in their bookshelf.
    There is can be only one public bookshelf in the system.
    There is can be only one Librarian in the system as well.
    The amount of public books in system is unlimited.

--- library/models.py
+++ library/models.py
@@ -1,6 +1,5 @@
 import uuid
 from django.db import models
-from django.utils import timezone
 from users.models import User
 
 
@@ -13,48 +12,138 @@ def upload_to(instance, filename) -> str:
     return f'books/{str(instance)}-{filename}'
 
 
-class Book(models.Model):
-    """Describes Book in our DB"""
+class Bookshelf(models.Model):
+    """
+    An entity to store books in to.
 
-    vk_id = models.ForeignKey(User, related_name='books_id', on_delete=models.CASCADE)
+    There are two types of bookshelf: user's private bookshelf and
+    library bookshelf. The last one is used to store public books in.
+    If user "borrows" book from this shelf, "Librarian" sets a
+    record in the LibraryJournal with a set of book_unique_id and
+    shelf_unique_id.
+    Note that there is no actual "Librarian" model here it's just a user
+    with a vk_id=0.
+    """
+
     unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    public = models.BooleanField(null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        # TODO: Check to have only one model with public=true
+        super().save()
+
+    def create_public_shelf(self):
+        if not User.objects.filter(vk_id=0).exists():
+            librarian = User.objects.create(vk_id=0)
+            librarian.save()
+
+        librarian = User.objects.filter(vk_id=0).first()
+        self.objects.create(public=True, user=librarian)
+        self.save()
+
+    def __str__(self):
+        if str(self.user) == 'Librarian':
+            return 'Public Bookshelf'
+
+        return f'Private Bookshelf: {self.user}'
+
+
+class LibrarianJournal(models.Model):
+    """
+    An entity to store records of "borrowed" by user books.
+    """
+
+    user_shelf = models.ForeignKey(Bookshelf, on_delete=models.CASCADE)
+    borrowed_book = models.ForeignKey('Book', on_delete=models.CASCADE)
+
+
+class Book(models.Model):
+    """
+    An entity to store information about a book.
+
+    This model describes two types of books, which basically aren't that
+    different from each other.
+    Private Books – are books user uploaded. Those books assigned to
+    user's bookshelf and can't be accessed by another user.
+    Public Books – are books Librarian uploaded. Those books assigned to
+    "Librarian" public bookshelf
+    """
+
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    shelf = models.ForeignKey('Bookshelf', on_delete=models.CASCADE)
     title = models.CharField(max_length=256)
     author = models.CharField(max_length=256)
+    category = models.ForeignKey('BookCategory', on_delete=models.CASCADE, null=True)
     pages = models.IntegerField()
     words = models.IntegerField()
     file = models.FileField(upload_to=upload_to)
-    edited = models.DateTimeField()
+    public = models.BooleanField(null=False)
+    edited = models.DateTimeField(auto_now=True)
 
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        # To make sure to remember time of every update.
-        self.edited = timezone.now()
-        return super().save()
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        # TODO: Check category for public or not book. If it's the public, there should be a category, not – otherwise.
+        super().save()
 
     def __str__(self):
-        return f'{self.unique_id}'
+        return f'{self.unique_id}: {self.author} - {self.title}'
+
+    def borrow(self, shelf):
+        if (not self.public or
+                LibrarianJournal.objects.filter(user_shelf=shelf, borrowed_book=self).exists()):
+            return
+
+        lib = LibrarianJournal.objects.create(user_shelf=shelf, borrowed_book=self)
+        lib.save()
+
+    def give_back(self, shelf):
+        if (not self.public or
+                not LibrarianJournal.objects.filter(user_shelf=shelf, borrowed_book=self).exists()):
+            return
+
+        LibrarianJournal.objects.filter(user_shelf=shelf, borrowed_book=self).delete()
 
     class Meta:
         ordering = ['edited']
-        unique_together = ('title', 'author', 'vk_id')
+        unique_together = ('title', 'author', 'shelf')
+
+
+class BookCategory(models.Model):
+    """
+    Describes categories of public books.
+    """
+
+    name = models.CharField(primary_key=True, max_length=256)
+    title = models.CharField(max_length=256)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
 
 
 class Statistics(models.Model):
     """Describes Statistics of book reading"""
 
-    book_id = models.OneToOneField(Book, on_delete=models.CASCADE, primary_key=True)
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    shelve = models.ForeignKey(Bookshelf, on_delete=models.CASCADE)
     percentage = models.FloatField(max_length=4)
     pages_read = models.IntegerField()
     words_read = models.IntegerField()
     average_speed = models.FloatField(max_length=6)
-    edited = models.DateTimeField()
-
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        # To make sure to remember time of every update.
-        self.edited = timezone.now()
-        return super().save()
+    edited = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.book_id} - {self.percentage}%'
 
+    def get_book_stat(self, user: User):
+        return
+
     class Meta:
         ordering = ['edited']
+        unique_together = ('book_id', 'shelve')
