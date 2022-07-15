267cd9da2d1c547ea77b56b5cf1b0349287e2a44
Petr <je09@yandex.ru>
2022-07-15 13:55:18 +0300

    upd: library models – bookshelf
    Added support for public and private type of books through implementing new bookshelf entity.
    The idea is to set relationship between user and bookshelf and through bookshelf store users books.
    Private books – ones user upload himself have a many-to-one relationship with a bookshelf.
     Public books – ones user "borrows" from our library have a many-to-many relationship with a bookshelf.

--- library/models.py
+++ library/models.py
@@ -14,28 +14,68 @@ def upload_to(instance, filename) -> str:
 
 
 class Book(models.Model):
-    """Describes Book in our DB"""
-
-    vk_id = models.ForeignKey(User, related_name='books_id', on_delete=models.CASCADE)
     unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     title = models.CharField(max_length=256)
     author = models.CharField(max_length=256)
     pages = models.IntegerField()
     words = models.IntegerField()
     file = models.FileField(upload_to=upload_to)
-    edited = models.DateTimeField()
-
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        # To make sure to remember time of every update.
-        self.edited = timezone.now()
-        return super().save()
+    edited = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.unique_id}'
+        return f'{self.unique_id}: {self.author} - {self.title}'
 
     class Meta:
+        abstract = True
         ordering = ['edited']
-        unique_together = ('title', 'author', 'vk_id')
+        unique_together = ('title', 'author')
+
+
+"""
+Public books
+
+Those books are free to use for every user.
+Bookshelf -> PublicBook
+"""
+
+
+class PublicBookCategory(models.Model):
+    shelf = models.ManyToManyField('Bookshelf')
+    name = models.CharField(primary_key=True, max_length=256)
+    title = models.CharField(max_length=256)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+
+class PublicBook(Book):
+    """Book which can be accessed by any user"""
+    category = models.ForeignKey(PublicBookCategory, on_delete=models.CASCADE)
+
+
+"""
+Private books are the ones User uploaded, they're allowed to use for this user only.
+
+PrivateBook -> Bookshelf
+"""
+
+
+class PrivateBook(Book):
+    """Book which can be accessed by user uploaded it only"""
+    shelf = models.ForeignKey('Bookshelf', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('title', 'author', 'shelf')
+
+
+class Bookshelf(models.Model):
+    """Entity where user stores his private and public books"""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    public_book = models.ManyToManyField(PublicBook)
 
 
 class Statistics(models.Model):
