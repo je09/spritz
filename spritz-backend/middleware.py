14d51b73f643a90b648e723717b883c45affb717
je09 <je09@yandex.ru>
2022-07-21 22:26:08 +0500

    upd: VKStartParam check for vk_user_id
    If vk_user_id isn't sent, drop connection and return Response Forbidden.

--- spritz-backend/middleware.py
+++ spritz-backend/middleware.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlparse, parse_qsl
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseForbidden
+from rest_framework import exceptions
 
 
 class VKStartParamParserMiddleware:
@@ -9,5 +10,8 @@ class VKStartParamParserMiddleware:
     def __call__(self, request: HttpRequest, *args, **kwargs):
         params = dict(parse_qsl(urlparse(request.META.get('HTTP_PARAMS')).query, keep_blank_values=True))
         for k, v in params.items(): request.META.setdefault(k, v)
+
+        if not request.META.get('vk_user_id'):
+            return HttpResponseForbidden(exceptions.PermissionDenied.default_detail)
 
         return self.get_response(request)
